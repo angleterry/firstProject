c9d6592e7e5c8afa4bea85b7aa96dc5cd7eca242
angleterry <shibukun@waqu.com>
2017-03-24 22:12:57 +0800

    添加greenDao

---
+++ app/src/main/java/com/android/firstproject/ui/fragment/BaseFragment.java
@@ -0,0 +1,10 @@
+package com.android.firstproject.ui.fragment;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by angleterry on 2017/3/23.
+ */
+
+public class BaseFragment extends Fragment {
+}
